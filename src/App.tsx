8bcc38759ad01c933387ea571ecc543fd10206b7
Vivek205 <ichbinvivek@gmail.com>
2024-10-25 23:30:55 +0200

    tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Header } from "./components/Header";
+// import "./css/global.css";
+
+export const App = () => {
+  return (
+    <div className="w-44 bg-red-500">
+      <Header />
+    </div>
+  );
+};
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+  return (
+    <div className="flex justify-start gap-x-2 w-full">
+      <p>Logo</p>
+      <p>ClipWise</p>
+    </div>
+  );
+};
