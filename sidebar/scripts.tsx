4b3af38b18054d743544f799388429d24dbb9223
Varun A P <8411309+apvarun@users.noreply.github.com>
2024-10-26 01:38:54 +0200

    migrate to using sidepanel

---
+++ sidebar/scripts.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SidePanelContent from "./src";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <SidePanelContent />
+  </React.StrictMode>
+);
---
+++ sidebar/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SidePanelContent = () => {
+  return (
+    <div>SidePanel Content</div>
+  )
+}
+
+export default SidePanelContent
