c3dff04c0f7170d3945cdcd889fe670918f98c33
Varun A P <8411309+apvarun@users.noreply.github.com>
2024-10-26 02:01:49 +0200

    run the content script from sidebar

--- sidebar/src/index.tsx
+++ sidebar/src/index.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const SidePanelContent = () => {
-  return (
-    <div>SidePanel Content</div>
-  )
-}
+  const [activeTab, setActiveTab] = useState<chrome.tabs.Tab | null>(null);
 
-export default SidePanelContent
+  useEffect(() => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      if (tabs[0]) {
+        console.log("tabs[0]", tabs[0]);
+        setActiveTab(tabs[0]);
+      }
+    });
+  }, []);
+
+  const handleClick = async () => {
+    if (activeTab?.id) {
+      try {
+        console.log("activeTab.id", activeTab.id);
+        alert("executing script");
+        await chrome.scripting.executeScript({
+          target: { tabId: activeTab.id },
+          files: ["scripts/content-script.js"],
+          world: "MAIN",
+        });
+        alert("executed script");
+      } catch (error) {
+        console.error("Error injecting script:", error);
+        alert(`An error occurred. Please try again. ${error}`);
+      }
+    } else {
+      alert("Please navigate to a YouTube video page to use this extension.");
+    }
+  };
+
+  return (
+    <div>
+      <h1>SidePanel Content</h1>
+      <button onClick={handleClick}>Click me</button>
+    </div>
+  );
+};
+
+export default SidePanelContent;
