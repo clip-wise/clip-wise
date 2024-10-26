cb228508f3f3bd77ea33bd07d87e25e7694a6886
Minh Pham <ngocminhpham66@gmail.com>
2024-10-26 10:26:32 +0200

    fix: remove duplicated Actions

--- constants.tsx
+++ constants.tsx
@@ -1,14 +1,7 @@
 export const ChromeMessageTypes = {
-  Clip: 'clip',
-  ClipResponse: 'clip-response',
-  Summarize: 'summarize',
-};
-
-export const Actions = {
-  Clip: 'clip',
-  TakeNotes: 'take-notes',
-  FlashCards: 'flashcards',
-  Summary: 'summary',
+  Clip: "clip",
+  ClipResponse: "clip-response",
+  Summarize: "summarize",
 };
 
 export const Actions = {
