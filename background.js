00562fcc607c135332dfab5fceee32a198761786
Varun A P <8411309+apvarun@users.noreply.github.com>
2024-10-26 03:24:06 +0200

    enable sidebar only for youtube

--- background.js
+++ background.js
@@ -26,3 +26,22 @@ chrome.runtime.onMessage.addListener(async function (message, sender, reply) {
     chrome.runtime.sendMessage({ type: "captions-to-skip", data: captions });
   }
 });
+
+chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
+  if (!tab.url) return;
+  const url = new URL(tab.url);
+  // Enables the side panel on google.com
+  if (url.origin === "https://www.youtube.com") {
+    await chrome.sidePanel.setOptions({
+      tabId,
+      path: "side_panel/default_path.html",
+      enabled: true,
+    });
+  } else {
+    // Disables the side panel on all other sites
+    await chrome.sidePanel.setOptions({
+      tabId,
+      enabled: false,
+    });
+  }
+});
